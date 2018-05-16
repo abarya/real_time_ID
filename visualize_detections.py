ddbbb56af6b7e915fc4f040ca7e288453ff0d143
abhishekarya286 <abhishekarya286@gmail.com>
2018-05-16 16:51:16 +0530

    new videos added

--- visualize_detections.py
+++ visualize_detections.py
@@ -62,7 +62,7 @@ def _main(args):
         video_file = os.path.expanduser(args.video_file)
         detections_file_path = os.path.expanduser(args.detections_file_path)
 
-        detection_file = 'result_test_video.out'#os.path.join(detections_file_path,video_file+'.out')
+        detection_file = video_file+'.out'#os.path.join(detections_file_path,video_file+'.out')
 
         with open(detection_file, "rb") as csvfile:
             lines = csv.reader(csvfile)
@@ -74,23 +74,30 @@ def _main(args):
         cap = cv2.VideoCapture(os.path.join(video_path,video_file+'.webm'))
         count=0
         i=0
+        correct=0
         while(1):
             ret, frame = cap.read()
             if ret==0:
                 print("video read completely\n")
                 break
-
             while(i<len(lines) and count==lines[i][0]):
-                cv2.rectangle(frame,(lines[i][1],lines[i][2]),(lines[i][3],lines[i][4]),(0,255,0),3)
-                cv2.putText(frame,global_var.CLASSES[lines[i][5]],(lines[i][1],lines[i][2]),cv2.FONT_HERSHEY_SIMPLEX, 4,(0,0,255),2,cv2.LINE_AA)
+                cv2.rectangle(frame,(lines[i][1],lines[i][2]),(lines[i][3],lines[i][4]),(0,255,0),1)
+                cv2.putText(frame,global_var.CLASSES[lines[i][5]],(lines[i][1],lines[i][2]),cv2.FONT_HERSHEY_SIMPLEX,1,(0,0,255),2,cv2.LINE_AA)
                 img = frame[lines[i][2]:lines[i][4],lines[i][1]:lines[i][3],:]
+                if global_var.CLASSES[lines[i][5]]==video_file:
+                    correct+=1
+                else:
+                    print global_var.CLASSES[lines[i][5]]
                 i+=1  
             frame = cv2.resize(frame,(frame.shape[1]/2,frame.shape[0]/2))  
             cv2.imshow("f",frame)
+            img_name = "{:05d}.jpg".format(count)
+            print img_name
+            cv2.imwrite("images_for_video/"+img_name,frame)
             if(cv2.waitKey(100)==27):
                 break
             count+=1
-
+        print "accuracy",float(correct)/i
 
 if __name__ == '__main__':
     _main(parser.parse_args())
