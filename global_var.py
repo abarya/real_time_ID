9551d5c5599b03ba5903d5b88bae0f782b77d7a2
abhishekarya286 <abhishekarya286@gmail.com>
2018-05-17 15:46:47 +0530

    train test created for new videos

--- global_var.py
+++ global_var.py
@@ -1,3 +1,3 @@
 #This file will contain global variables that will be shared across multiple modules
-CLASSES = ['agniv','abhishek','anadi','suyash']
-classes_dict = {'agniv':0,'abhishek':1,'anadi':2,'suyash':3}
+CLASSES = ['agniv','abhishek','anadi','suyash','pankaj','sagnik','nikhil','sahib','utkarsh','raghav']
+classes_dict = {'agniv':0,'abhishek':1,'anadi':2,'suyash':3,'pankaj':4,'sagnik':5,'nikhil':6,'sahib':7,'utkarsh':8,'raghav':9}
