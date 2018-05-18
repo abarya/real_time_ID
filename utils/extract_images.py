085b80b292bba78d11388159c8b6fe74a3587c2e
abhishekarya286 <abhishekarya286@gmail.com>
2018-05-18 18:52:25 +0530

    split train test for 21 persons

--- utils/extract_images.py
+++ utils/extract_images.py
@@ -7,10 +7,6 @@ parser = argparse.ArgumentParser(
     description='Visualization of detections in test videos')
 
 parser.add_argument(
-    'video_file',
-    help='name of the video file to process')
-
-parser.add_argument(
     '--video_path',
     help='path to directory of of video files',
     default='../data')
@@ -23,48 +19,52 @@ parser.add_argument(
 
 def _main(args):
     video_path = os.path.expanduser(args.video_path)
-    video_file = os.path.expanduser(args.video_file)
     detections_file_path = os.path.expanduser(args.detections_file_path)
 
-    result_dir = os.path.join('../data',video_file)
-    detection_file = os.path.join(detections_file_path,video_file+'.out')
-    detection_file = '../'+detection_file
+    for video_file in os.listdir(video_path):
+        if os.path.isdir(os.path.join(video_path,video_file)):
+            continue
+        print os.path.join(os.getcwd(),video_file),"arya",os.path.isdir(os.path.join(os.getcwd(),video_file))
+        result_dir = os.path.join('../data',os.path.splitext(video_file)[0])
+        detection_file = os.path.join(detections_file_path,os.path.splitext(video_file)[0]+'.out')
+        detection_file = '../'+detection_file
 
-    with open(detection_file, "rb") as csvfile:
-    	lines = csv.reader(csvfile)
-    	lines = list(lines)
-    	for i in range(len(lines)):
-    		for j in range(5):
-    			lines[i][j]=int(float(lines[i][j]))
+        with open(detection_file, "rb") as csvfile:
+        	lines = csv.reader(csvfile)
+        	lines = list(lines)
+        	for i in range(len(lines)):
+        		for j in range(5):
+        			lines[i][j]=int(float(lines[i][j]))
 
-    if not os.path.exists(result_dir):
-        os.mkdir(result_dir)
+        if not os.path.exists(result_dir):
+            os.mkdir(result_dir)
 
-    cap = cv2.VideoCapture(os.path.join(video_path,video_file+'.webm'))
-    count=0
-    i=0
-    img_count = 0
-    while(1):
-		ret, frame = cap.read()
-		if ret==0:
-			print("video read completely\n")
-			break
+        cap = cv2.VideoCapture(os.path.join(video_path,video_file))
+        count=0
+        i=0
+        img_count = 0
+        while(1):
+    		ret, frame = cap.read()
+    		if ret==0:
+    			print("video read completely\n")
+    			break
 
-		while(i<len(lines) and count==lines[i][0]):
-			#save detections after 20 frames
-			if count%20==0:
-				img = frame[lines[i][2]:lines[i][4],lines[i][1]:lines[i][3],:]
-				img_name = "{:04d}.png".format(img_count)
-				cv2.imwrite(os.path.join(result_dir,img_name),img)
-				img_count+=1
-			cv2.rectangle(frame,(lines[i][1],lines[i][2]),(lines[i][3],lines[i][4]),(0,255,0),3)
-			i+=1
+    		while(i<len(lines) and count==lines[i][0]):
+    			#save detections after 20 frames
+    			if count%20==0:
+    				img = frame[lines[i][2]:lines[i][4],lines[i][1]:lines[i][3],:]
+    				img_name = "{:04d}.png".format(img_count)
+    				cv2.imwrite(os.path.join(result_dir,img_name),img)
+    				img_count+=1
+    			cv2.rectangle(frame,(lines[i][1],lines[i][2]),(lines[i][3],lines[i][4]),(0,255,0),3)
+    			i+=1
 
-		frame = cv2.resize(frame,(frame.shape[1]/2,frame.shape[0]/2))  
-		cv2.imshow("f",frame)
-		if(cv2.waitKey(1)==27):
-			break
-		count+=1
+    		frame = cv2.resize(frame,(frame.shape[1]/2,frame.shape[0]/2))  
+    		cv2.imshow("f",frame)
+    		if(cv2.waitKey(1)==27):
+    			break
+    		count+=1
+        cap.release()
 
 
 if __name__ == '__main__':
