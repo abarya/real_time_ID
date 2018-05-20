371c9ecd8f8178395917cf84b3a6584583f18b25
abhishekarya286 <abhishekarya286@gmail.com>
2018-05-21 00:45:36 +0530

    VIPeR added

--- utils/draw_cmc.py
+++ utils/draw_cmc.py
@@ -2,7 +2,7 @@ from matplotlib import pyplot as plt
 
 
 viper = plt.plot([0,1,2,5,10],[0,22,32,47,60],'ro-',label='VIPeR')
-magi = plt.plot([0,1,2,5,10],[0,92,94,95,96.6],'go-',label='MAGI')
+magi = plt.plot([0,1,2,5,8,10,12,15,18,20],[0,83, 90, 96, 98, 98, 99, 100, 100, 100],'go-',label='MAGI')
 ilid = plt.plot([0,1,2,5,10],[0,95,96,97,99],'bo-',label='ILID')
 plt.legend(framealpha=0.5)
 plt.grid(True)
