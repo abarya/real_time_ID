fc29310c17ba5f481137e5815867b97bb90dbe45
abhishekarya286 <abhishekarya286@gmail.com>
2018-05-25 02:14:13 +0530

    minor nits

--- test_yolo.py
+++ test_yolo.py
@@ -218,7 +218,7 @@ def _main(args):
                 bottom = min(image.size[1], np.floor(bottom + 0.5).astype('int32'))
                 right = min(image.size[0], np.floor(right + 0.5).astype('int32'))
                 # print(label, (left, top), (right, bottom),c)
-                if c==0 and score>0.6:
+                if c==0 and score>0.55:
                     # print("person detected")
                     num_detections+=1
                     bBoxesList.append(np.array([count-1,left,top,right,bottom]))
